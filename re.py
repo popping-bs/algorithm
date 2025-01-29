78d87759cd6f040f65ee6b39427f8c4ea45eaca2
popping-bs <poppinbs0714@gmail.com>
2025-01-29 18:21:12 +0900

    Update re.py

--- re.py
+++ re.py
@@ -6,6 +6,7 @@ value =0
 
 
 for x in data:
+    #알파벳인 경우 결과 리스트에 삽입
     if x.isalpha():
         result.append(x)
 
@@ -13,7 +14,7 @@ for x in data:
         value += int(x)
 
 result.sort()
-
+#숫자가 하나라도 존재하는 경우 가장 뒤에 삽임
 if value != 0:
     result.append(str(value))
 
