f3516051ed03cb6a4a7760f9440fdcf79f5e39ff
bouncekim <poppinbs0714@gmail.com>
2025-01-29 17:39:11 +0900

    두번째파일

---
+++ 모험가길드.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+data = list(map(int, input().split()))
+data.sort()
+
+result = 0
+count = 0
+
+# 5 5 5 5 6 7
+for i in data:
+    count += 1
+    if count >= i:
+        result +=1
+        count = 0
+
+
+print(result)
+
+
+
