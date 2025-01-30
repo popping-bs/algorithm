deb6adb03dac3eb0329d3bd5042d20d5f5eb5772
bouncekim <poppinbs0714@gmail.com>
2025-01-30 16:44:39 +0900

    알고리즘 공부했던내용

--- 115page.py
+++ 115page.py
@@ -4,7 +4,7 @@
 
 input_data = input()
 row = int(input_data[1])
-column = int(ord(input_data[0]) - int(ord('a'))) + 1
+column = int(ord(input_data[0])) - int(ord('a')) + 1
 
 steps = [(-2,1), (-1,-2),(1,-2),(2,-1),(2,1),(1,2),(-1,2),(-2,1)]
 
